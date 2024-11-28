e700bd711d079502148d2f38e079e5334c8f9b79
Ben Koshy <benkoshy@hotmail.com>
2024-11-28 12:55:58 +1100

    add: a second elm app, change index.html to refer to dist/index.js file
    to run:
    
    * yarn build watch
    * and then to go a web browswer
    
    Expectation:
    
    * for both elm apps to show.
    
    Actuality:
    
    * only one elm app is showing.

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,3 +1,5 @@
+
+
 import { Elm } from './Main.elm';
 
 const $root = document.createElement('div');
@@ -7,3 +9,18 @@ Elm.Main.init({
   node: $root
 });
 
+
+
+import { Elm2 } from './Main2.elm';
+
+
+document.createElement('br');
+document.createElement('br');
+document.createElement('br');
+
+const $root2 = document.createElement('div');
+document.body.appendChild($root2);
+
+Elm2.Main2.init({
+  node: $root2
+});
